e36d030b91ccb2b50ce0000067dee96c8d0b43fc
pvloc <phamvanloc2301@gmail.com>
2024-02-23 15:17:58 +0700

    cm1

---
+++ bainhomhc/timkiem/search.js
@@ -0,0 +1,30 @@
+const categories = [...new Set(product.map((item) => { return item }))]
+
+document.getElementById('searchBar').addEventListener('keyup', (e) => {
+    const searchData = e.target.value.toLowerCase();
+    const filteredData = categories.filter((item) => {
+        return (
+            item.title.toLowerCase().includes(searchData)
+        )
+    })
+    displayItem(filteredData)
+});
+
+const displayItem = (items) => {
+    document.getElementById('root').innerHTML = items.map((item) => {
+        var { image, title, price } = item;
+        return (
+            `<div class='box'>
+                <div class='img-box'>
+                    <img class='images' src=${image}></img>
+                </div> 
+                <div class='bottom'>
+                    <p>${title}</p>
+                    <h2> Giá ${price}.000đ</h2>
+                <button>Add to cart</button>
+                </div>
+            </div>`
+        )
+    }).join('')
+};
+displayItem(categories);
